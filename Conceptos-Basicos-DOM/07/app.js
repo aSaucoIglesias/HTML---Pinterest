9bb1c3c65ff4dae7aaf6239b7472246a6fe4abce
sauco-phi <antoniosauco@facephi.com>
2025-09-02 21:47:43 +0200

    solución error

--- Conceptos-Basicos-DOM/07/app.js
+++ Conceptos-Basicos-DOM/07/app.js
@@ -1,10 +1,11 @@
 const places = ["Gondor", "Mordor", "Rivendel", "La Comarca", "Nümenor"];
 const container = document.querySelector('[data-function="printHere"]');
 const ul = document.createElement("ul");
-document.body.appendChild(ul);
 
 for (const place of places) {
     const li = document.createElement("li");
     li.textContent = place;
     ul.appendChild(li)
 }
+
+container.appendChild(ul);
